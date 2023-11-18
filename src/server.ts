cbaa8c5bc07c727b1097e5e62d63f4305bc69f33
NavarroGuimaraes <navasxll@hotmail.com>
2023-11-18 12:10:33 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import fastify from 'fastify'
+import { knex } from './database'
+
+const app = fastify()
+
+app.get('/tables', async (request, reply) => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then((address) => {
+    console.log(`server listening on ${address}`)
+  })
+  .catch((err) => {
+    console.error(err)
+  })
