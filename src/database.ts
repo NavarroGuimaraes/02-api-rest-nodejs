d89269c36bc22f29f0072c3306dbda0f6621a440
NavarroGuimaraes <navasxll@hotmail.com>
2024-01-11 17:23:09 -0300

    third class

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,15 @@
-import { knex as setupKnex } from 'knex'
+import { knex as setupKnex, Knex } from "knex";
 
-export const knex = setupKnex({
-  client: 'sqlite3',
-  connection: {
-    filename: './tmp/database.sqlite',
-  },
-})
+export const dbConfig: Knex.Config = {
+    client:"sqlite3",
+    connection: {
+        filename: "./domain/database.sqlite",
+    },
+    useNullAsDefault: true,
+    migrations: {
+        extension: "ts",
+        directory: "./domain/migrations",
+    },
+};
+
+export const knex = setupKnex(dbConfig);
